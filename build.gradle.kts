fc115b67579d101ec0f3178b2a4f48147f0ec5ad
Pavlo Moisieienko <pavel.moyseyenko@yahoo.com>
2023-09-27 12:35:27 +0300

    new: gradle properties and fix deprecated

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,10 @@ allprojects {
     }
 
     dependencies {
-        testImplementation(platform("org.junit:junit-bom:5.10.0"))
-        testImplementation("org.junit.jupiter:junit-jupiter")
+//        testImplementation(platform("org.junit:junit-bom:5.10.0"))
+//        testRuntimeOnly("org.junit.jupiter:junit-jupiter")
+        testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     }
     tasks.test {
         useJUnitPlatform()
